8228de0d316078a927ee0c2c1de766d8e5e1633a
Ruibin Xi <xiruibing@126.com>
2014-02-09 22:58:33 +0800

    增加登陆验证
    登录验证，相册照片列表

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/main', routes.main);
 app.get('/users', user.list);
+app.post('/user/login', user.login);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,12 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+exports.login = function(req, res){
+    if (req.body.username=='admin' && req.body.password=='admin') {
+//        res.send('<script>alert("登录成功.")</script>');
+        res.redirect('main');
+    } else {
+        res.send('<script>alert("账号错误.");history.go(-1);</script>');
+    }
+
+};
